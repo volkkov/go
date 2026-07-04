5917381a8e589bee17980e83e1f004ebe703c3fa
zomeiki <88155358+Zganz404@users.noreply.github.com>
2026-07-04 10:35:07 +0200

    Create emailcheck.go
    Add emailcheck.go with regex email validation
    
    Co-authored-by: Alexsei Volkov <102011945+volkkov@users.noreply.github.com>

---
+++ emailcheck.go
@@ -0,0 +1,9 @@
+package main
+
+import "regexp"
+
+// IsEmailFormatValid checks email format using regex
+func IsEmailFormatValid(email string) bool {
+	re := regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`)
+	return re.MatchString(email)
+}
